8e37e9f6f14f38cb866293fbd23a34149d669418
Nikola Mitrovic <mnikola300@gmail.com>
2023-11-30 13:19:50 +0100

    made reviews bars and comment section

--- src/app/accommodation-details/comments/comments.component.ts
+++ src/app/accommodation-details/comments/comments.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrl: './comments.component.css'
+  styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent {
+  comments = [
+   { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+    { name: 'John Doe', sentAt: new Date(), image: 'assets/BG.jpg', commentText: 'This is a comment.' },
+  ];
 
+  displayedComments: any[];
+
+  constructor() {
+    this.displayedComments = this.comments.slice(0, 10); //
+  }
+
+  loadMoreComments() {
+    const currentLength = this.displayedComments.length;
+    const remainingComments = this.comments.slice(currentLength, currentLength + 5);
+    this.displayedComments = [...this.displayedComments, ...remainingComments];
+  }
 }
