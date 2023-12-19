203c1884eed4593c85bf98ddd6d1277e5e84135d
Nikola Mitrovic <mnikola300@gmail.com>
2023-12-19 15:13:22 +0100

    connected accommodation details and validation for reservations

--- src/app/accommodation-details/location/location.component.ts
+++ src/app/accommodation-details/location/location.component.ts
@@ -1,22 +1,32 @@
-import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import {Component, AfterViewInit, Inject, PLATFORM_ID, Input} from '@angular/core';
 import * as L from 'leaflet';
 import { isPlatformBrowser } from "@angular/common";
+import {LocationService} from "./location.service";
 
+interface  Location{
+  address:string,
+  city:string,
+  country:string
+}
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements AfterViewInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  @Input() location!: Location;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+              private locationService: LocationService) {}
 
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       setTimeout(() => {
+
         this.loadLeaflet();
       }, 0);
     }
   }
+
 
   private loadLeaflet() {
     import('leaflet').then((L) => {
@@ -27,14 +37,36 @@ export class LocationComponent implements AfterViewInit {
   }
 
   private initializeMap(L: any) {
+    let DefaultIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png',
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
     const map = L.map('mapContainer').setView([51.505, -0.09], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
-
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A sample location.')
-      .openPopup();
+    const searchAddress = (address: string) => {
+      this.locationService.search(address).subscribe(
+        (data: any) => {
+          if (data && data.length > 0) {
+            const { lat, lon } = data[0]; // Assuming the first result
+            map.setView([lat, lon], 13);
+            L.marker([lat, lon]).addTo(map)
+              .bindPopup('Searched location: ' + address)
+              .openPopup();
+          } else {
+            console.error('Location not found');
+          }
+        },
+        (error) => {
+          console.error('Error occurred while searching:', error);
+        }
+      );
+    };
+    //this.location.address='Hilandarska 1';
+    //this.location.city='Novi Sad'
+    searchAddress(this.location.address+", "+this.location.city);
   }
 }
