7eaae17ea22470b889914b75a4bbdeeef7aff287
marijazhiv <105136036+marijazhiv@users.noreply.github.com>
2024-01-07 01:08:44 +0100

    amenities loaded

--- src/app/create-accommodation/create-accommodation.component.ts
+++ src/app/create-accommodation/create-accommodation.component.ts
@@ -3,9 +3,6 @@ import { isPlatformBrowser } from "@angular/common";
 import {AccommodationService} from "./createaccommodation.service";
 import {HttpClient} from "@angular/common/http";
 import { FormsModule } from '@angular/forms';
-
-
-
 @Component({
   selector: 'app-create-accommodation',
   templateUrl: './create-accommodation.component.html',
@@ -15,10 +12,7 @@ export class CreateAccommodationComponent implements AfterViewInit{
   enteredAddress: string = '';
   map: any;
   marker: any;
-
-
   constructor(@Inject(PLATFORM_ID) private platformId: Object,private http: HttpClient, private accommodationService: AccommodationService) {}
-
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       setTimeout(() => {
@@ -26,19 +20,14 @@ export class CreateAccommodationComponent implements AfterViewInit{
       }, 0);
     }
   }
-
-  //dto
   name: string = '';
   description: string = '';
   type: string = '';
   minNumberOfGuests: any;
-  //amenities: [ ] ;
   maxNumberOfGuests: any;
   cancellationDeadline: any;
-  //enteredAddress: string='';
-
+  price:number;
   specialPrices: { startDate: any, endDate: any, amount: number }[] = [];
-
     addSpecialPrice() {
     this.specialPrices.push({
       startDate: '',
@@ -46,11 +35,9 @@ export class CreateAccommodationComponent implements AfterViewInit{
       amount: null
     });
   }
-
   removeSpecialPrice(index: number) {
     this.specialPrices.splice(index, 1);
   }
-
   amenities = {
     amenity1: false,
     wifi: false,
@@ -58,28 +45,8 @@ export class CreateAccommodationComponent implements AfterViewInit{
     gymCenter: false,
     videoGames: false
   };
-
-  accommodationData: any = {};
-  selectedImages: { url: string, file: File }[] = [];
-
-
-  handleImageUpload(event: any) {
-    this.selectedImages = [];
-
-
-    const files: FileList | null = event.target.files;
-
-    if (files) {
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const url = URL.createObjectURL(file);
-        this.selectedImages.push({ url, file });
-      }
-    }
-  }
-
-
-
+  //accommodationData: any = {};
+  //selectedImages: { url: string, file: File }[] = [];
   selectedImage: { url: string, file: File } = null;
   fileUploaded: boolean = false;
   avatarBytes: string;
@@ -100,24 +67,18 @@ export class CreateAccommodationComponent implements AfterViewInit{
     this.fileUploaded = true;
   }
   accommodationId: number;
-
-  currentStep: string = 'createAccommodation';
   onSubmit() {
-
     const availabilityData = this.specialPrices.map(specialPrice => {
       return {
         start: specialPrice.startDate,
         end: specialPrice.endDate,
-        specialPrice: specialPrice.amount
+        specialPrice:  specialPrice.amount
       };
     });
-
     const selectedAmenities = [];
-
     if (this.amenities.amenity1) {
       selectedAmenities.push({ "id": 1 });
     }
-
     if (this.amenities.wifi) {
       selectedAmenities.push({ "id": 3 });
     }
@@ -130,9 +91,7 @@ export class CreateAccommodationComponent implements AfterViewInit{
     if (this.amenities.videoGames) {
       selectedAmenities.push({ "id": 6 });
     }
-
     const accommodationData = {
-
       name: this.name,
       description: this.description,
       location: {
@@ -141,24 +100,22 @@ export class CreateAccommodationComponent implements AfterViewInit{
       address: this.enteredAddress.split(',')[0].trim()
     },
       type: this.type,
-      images: ["image4.jpg", "image5.jpg"],
+       images: ["image1.jpg" ],
       amenities: selectedAmenities,
       rating: 5.0,
       minNumberOfGuests:this.minNumberOfGuests,
       maxNumberOfGuests: this.maxNumberOfGuests,
       availability: availabilityData,
-      pricePerNight : 1200.0,
+      pricePerNight : this.price,
      pricePerGuest : true,
       cancellationDeadline:this.cancellationDeadline
     };
-
-    //if (this.currentStep === 'createAccommodation') {
     this.accommodationService.addAccommodation(accommodationData).subscribe(
       (response) => {
         this.accommodationId = response;
         console.log('Accommodation added successfully', response );
 
-        alert('Accommodation added successfully! Request sent to admin!'+ response);
+        alert('Accommodation added successfully! Request sent to admin!');
         //this.currentStep = 'upload';
         if (this.fileUploaded) {
           this.http.post(`http://localhost:8080/api/accommodations/${this.accommodationId}/image`, this.avatarBytes, { responseType: 'text' }).subscribe({
@@ -176,10 +133,7 @@ export class CreateAccommodationComponent implements AfterViewInit{
         console.error('Error adding accommodation', error);
       }
     );
-
-
   }
-
   private loadLeaflet() {
     import('leaflet').then((L) => {
       this.initializeMap(L);
@@ -187,7 +141,6 @@ export class CreateAccommodationComponent implements AfterViewInit{
       console.error('Leaflet failed to load', err);
     });
   }
-
   private initializeMap(L: any) {
     const map = L.map('mapContainer').setView([51.505, -0.09], 13);
 
@@ -198,27 +151,19 @@ export class CreateAccommodationComponent implements AfterViewInit{
     L.marker([51.5, -0.09]).addTo(map)
       .bindPopup('A sample location.')
       .openPopup();
-
-    // Initialize marker without setting its location
     this.marker = L.marker([0, 0]).addTo(this.map);
   }
-
   locateOnMap() {
-    // Geocode the entered address to get its coordinates
-    // For simplicity, you can use a geocoding service/library like OpenCage Geocoding API
-    // Replace 'YOUR_API_KEY' with your actual API key
+
     const apiKey = '7234404387b94ae4bdc2c7d6bb31bf58';
     const geocodingUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(this.enteredAddress)}&key=${apiKey}`;
 
     fetch(geocodingUrl)
       .then(response => response.json())
       .then(data => {
-        // Check if the geocoding was successful and has results
         if (data.results && data.results.length > 0) {
           const location = data.results[0].geometry;
           const { lat, lng } = location;
-
-          // Update the marker's location and reposition the map
           this.marker.setLatLng([lat, lng]);
           this.map.setView([lat, lng], 13);
         } else {
@@ -229,8 +174,4 @@ export class CreateAccommodationComponent implements AfterViewInit{
         console.error('Error during geocoding', error);
       });
   }
-
-
-
-
 }
